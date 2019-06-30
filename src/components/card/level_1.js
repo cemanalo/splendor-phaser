723223be5e60cfd32ec05f0d9ec25aed7d1cd050
Eugene.Manalo <eugene.manalo@EugeneManalos-MacBook-Pro.local>
2019-06-30 20:00:26 +0800

    Added data files for cards

---
+++ src/components/card/level_1.js
@@ -0,0 +1,363 @@
+// converted from https://csv.keyangxiang.com/#reviewResult
+export default [
+  {
+    "discount": "onyx",
+    "points": "0",
+    "onyx": "0",
+    "diamond": "1",
+    "ruby": "1",
+    "sapphire": "1",
+    "emerald": "1"
+  },
+  {
+    "discount": "onyx",
+    "points": "0",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "1",
+    "sapphire": "0",
+    "emerald": "2"
+  },
+  {
+    "discount": "onyx",
+    "points": "0",
+    "onyx": "0",
+    "diamond": "2",
+    "ruby": "0",
+    "sapphire": "0",
+    "emerald": "2"
+  },
+  {
+    "discount": "onyx",
+    "points": "0",
+    "onyx": "1",
+    "diamond": "0",
+    "ruby": "3",
+    "sapphire": "0",
+    "emerald": "1"
+  },
+  {
+    "discount": "onyx",
+    "points": "0",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "0",
+    "sapphire": "0",
+    "emerald": "3"
+  },
+  {
+    "discount": "onyx",
+    "points": "0",
+    "onyx": "0",
+    "diamond": "1",
+    "ruby": "1",
+    "sapphire": "2",
+    "emerald": "1"
+  },
+  {
+    "discount": "onyx",
+    "points": "0",
+    "onyx": "0",
+    "diamond": "2",
+    "ruby": "1",
+    "sapphire": "2",
+    "emerald": "0"
+  },
+  {
+    "discount": "onyx",
+    "points": "1",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "0",
+    "sapphire": "4",
+    "emerald": "0"
+  },
+  {
+    "discount": "sapphire",
+    "points": "0",
+    "onyx": "2",
+    "diamond": "1",
+    "ruby": "0",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "sapphire",
+    "points": "0",
+    "onyx": "1",
+    "diamond": "1",
+    "ruby": "2",
+    "sapphire": "0",
+    "emerald": "1"
+  },
+  {
+    "discount": "sapphire",
+    "points": "0",
+    "onyx": "1",
+    "diamond": "1",
+    "ruby": "1",
+    "sapphire": "0",
+    "emerald": "1"
+  },
+  {
+    "discount": "sapphire",
+    "points": "0",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "1",
+    "sapphire": "1",
+    "emerald": "3"
+  },
+  {
+    "discount": "sapphire",
+    "points": "0",
+    "onyx": "3",
+    "diamond": "0",
+    "ruby": "0",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "sapphire",
+    "points": "0",
+    "onyx": "0",
+    "diamond": "1",
+    "ruby": "2",
+    "sapphire": "0",
+    "emerald": "2"
+  },
+  {
+    "discount": "sapphire",
+    "points": "0",
+    "onyx": "2",
+    "diamond": "0",
+    "ruby": "0",
+    "sapphire": "0",
+    "emerald": "2"
+  },
+  {
+    "discount": "sapphire",
+    "points": "1",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "4",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "emerald",
+    "points": "0",
+    "onyx": "0",
+    "diamond": "2",
+    "ruby": "0",
+    "sapphire": "1",
+    "emerald": "0"
+  },
+  {
+    "discount": "emerald",
+    "points": "0",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "2",
+    "sapphire": "2",
+    "emerald": "0"
+  },
+  {
+    "discount": "emerald",
+    "points": "0",
+    "onyx": "0",
+    "diamond": "1",
+    "ruby": "0",
+    "sapphire": "3",
+    "emerald": "1"
+  },
+  {
+    "discount": "emerald",
+    "points": "0",
+    "onyx": "1",
+    "diamond": "1",
+    "ruby": "1",
+    "sapphire": "1",
+    "emerald": "0"
+  },
+  {
+    "discount": "emerald",
+    "points": "0",
+    "onyx": "2",
+    "diamond": "1",
+    "ruby": "1",
+    "sapphire": "1",
+    "emerald": "0"
+  },
+  {
+    "discount": "emerald",
+    "points": "0",
+    "onyx": "2",
+    "diamond": "0",
+    "ruby": "2",
+    "sapphire": "1",
+    "emerald": "0"
+  },
+  {
+    "discount": "emerald",
+    "points": "0",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "3",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "emerald",
+    "points": "1",
+    "onyx": "4",
+    "diamond": "0",
+    "ruby": "0",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "ruby",
+    "points": "0",
+    "onyx": "0",
+    "diamond": "3",
+    "ruby": "0",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "ruby",
+    "points": "0",
+    "onyx": "3",
+    "diamond": "1",
+    "ruby": "1",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "ruby",
+    "points": "0",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "0",
+    "sapphire": "2",
+    "emerald": "1"
+  },
+  {
+    "discount": "ruby",
+    "points": "0",
+    "onyx": "2",
+    "diamond": "2",
+    "ruby": "0",
+    "sapphire": "0",
+    "emerald": "1"
+  },
+  {
+    "discount": "ruby",
+    "points": "0",
+    "onyx": "1",
+    "diamond": "2",
+    "ruby": "0",
+    "sapphire": "1",
+    "emerald": "1"
+  },
+  {
+    "discount": "ruby",
+    "points": "0",
+    "onyx": "1",
+    "diamond": "1",
+    "ruby": "0",
+    "sapphire": "1",
+    "emerald": "1"
+  },
+  {
+    "discount": "ruby",
+    "points": "0",
+    "onyx": "0",
+    "diamond": "2",
+    "ruby": "2",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "ruby",
+    "points": "1",
+    "onyx": "0",
+    "diamond": "4",
+    "ruby": "0",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "diamond",
+    "points": "0",
+    "onyx": "1",
+    "diamond": "0",
+    "ruby": "0",
+    "sapphire": "2",
+    "emerald": "2"
+  },
+  {
+    "discount": "diamond",
+    "points": "0",
+    "onyx": "1",
+    "diamond": "0",
+    "ruby": "2",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "diamond",
+    "points": "0",
+    "onyx": "1",
+    "diamond": "0",
+    "ruby": "1",
+    "sapphire": "1",
+    "emerald": "1"
+  },
+  {
+    "discount": "diamond",
+    "points": "0",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "0",
+    "sapphire": "3",
+    "emerald": "0"
+  },
+  {
+    "discount": "diamond",
+    "points": "0",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "0",
+    "sapphire": "2",
+    "emerald": "2"
+  },
+  {
+    "discount": "diamond",
+    "points": "0",
+    "onyx": "1",
+    "diamond": "0",
+    "ruby": "1",
+    "sapphire": "1",
+    "emerald": "2"
+  },
+  {
+    "discount": "diamond",
+    "points": "0",
+    "onyx": "1",
+    "diamond": "3",
+    "ruby": "0",
+    "sapphire": "1",
+    "emerald": "0"
+  },
+  {
+    "discount": "diamond",
+    "points": "1",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "0",
+    "sapphire": "0",
+    "emerald": "4"
+  }
+]
---
+++ src/components/card/level_2.js
@@ -0,0 +1,273 @@
+// converted from https://csv.keyangxiang.com/#reviewResult
+export default [
+  {
+    "discount": "onyx",
+    "points": "1",
+    "onyx": "0",
+    "diamond": "3",
+    "ruby": "0",
+    "sapphire": "2",
+    "emerald": "2"
+  },
+  {
+    "discount": "onyx",
+    "points": "1",
+    "onyx": "2",
+    "diamond": "3",
+    "ruby": "0",
+    "sapphire": "0",
+    "emerald": "3"
+  },
+  {
+    "discount": "onyx",
+    "points": "2",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "2",
+    "sapphire": "1",
+    "emerald": "4"
+  },
+  {
+    "discount": "onyx",
+    "points": "2",
+    "onyx": "0",
+    "diamond": "5",
+    "ruby": "0",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "onyx",
+    "points": "2",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "3",
+    "sapphire": "0",
+    "emerald": "5"
+  },
+  {
+    "discount": "onyx",
+    "points": "3",
+    "onyx": "6",
+    "diamond": "0",
+    "ruby": "0",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "sapphire",
+    "points": "1",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "3",
+    "sapphire": "2",
+    "emerald": "2"
+  },
+  {
+    "discount": "sapphire",
+    "points": "1",
+    "onyx": "3",
+    "diamond": "0",
+    "ruby": "0",
+    "sapphire": "2",
+    "emerald": "3"
+  },
+  {
+    "discount": "sapphire",
+    "points": "2",
+    "onyx": "0",
+    "diamond": "5",
+    "ruby": "0",
+    "sapphire": "3",
+    "emerald": "0"
+  },
+  {
+    "discount": "sapphire",
+    "points": "2",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "0",
+    "sapphire": "5",
+    "emerald": "0"
+  },
+  {
+    "discount": "sapphire",
+    "points": "2",
+    "onyx": "4",
+    "diamond": "2",
+    "ruby": "1",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "sapphire",
+    "points": "3",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "0",
+    "sapphire": "6",
+    "emerald": "0"
+  },
+  {
+    "discount": "emerald",
+    "points": "1",
+    "onyx": "0",
+    "diamond": "3",
+    "ruby": "3",
+    "sapphire": "0",
+    "emerald": "2"
+  },
+  {
+    "discount": "emerald",
+    "points": "1",
+    "onyx": "2",
+    "diamond": "2",
+    "ruby": "0",
+    "sapphire": "3",
+    "emerald": "0"
+  },
+  {
+    "discount": "emerald",
+    "points": "2",
+    "onyx": "1",
+    "diamond": "4",
+    "ruby": "0",
+    "sapphire": "2",
+    "emerald": "0"
+  },
+  {
+    "discount": "emerald",
+    "points": "2",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "0",
+    "sapphire": "0",
+    "emerald": "5"
+  },
+  {
+    "discount": "emerald",
+    "points": "2",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "0",
+    "sapphire": "5",
+    "emerald": "3"
+  },
+  {
+    "discount": "emerald",
+    "points": "3",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "0",
+    "sapphire": "0",
+    "emerald": "6"
+  },
+  {
+    "discount": "ruby",
+    "points": "1",
+    "onyx": "3",
+    "diamond": "0",
+    "ruby": "2",
+    "sapphire": "3",
+    "emerald": "0"
+  },
+  {
+    "discount": "ruby",
+    "points": "1",
+    "onyx": "3",
+    "diamond": "2",
+    "ruby": "2",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "ruby",
+    "points": "2",
+    "onyx": "0",
+    "diamond": "1",
+    "ruby": "0",
+    "sapphire": "4",
+    "emerald": "2"
+  },
+  {
+    "discount": "ruby",
+    "points": "2",
+    "onyx": "5",
+    "diamond": "3",
+    "ruby": "0",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "ruby",
+    "points": "2",
+    "onyx": "5",
+    "diamond": "0",
+    "ruby": "0",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "ruby",
+    "points": "3",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "6",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "diamond",
+    "points": "1",
+    "onyx": "2",
+    "diamond": "0",
+    "ruby": "2",
+    "sapphire": "0",
+    "emerald": "3"
+  },
+  {
+    "discount": "diamond",
+    "points": "1",
+    "onyx": "0",
+    "diamond": "2",
+    "ruby": "3",
+    "sapphire": "3",
+    "emerald": "0"
+  },
+  {
+    "discount": "diamond",
+    "points": "2",
+    "onyx": "2",
+    "diamond": "0",
+    "ruby": "4",
+    "sapphire": "0",
+    "emerald": "1"
+  },
+  {
+    "discount": "diamond",
+    "points": "2",
+    "onyx": "0",
+    "diamond": "0",
+    "ruby": "5",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "diamond",
+    "points": "2",
+    "onyx": "3",
+    "diamond": "0",
+    "ruby": "5",
+    "sapphire": "0",
+    "emerald": "0"
+  },
+  {
+    "discount": "diamond",
+    "points": "3",
+    "onyx": "0",
+    "diamond": "6",
+    "ruby": "0",
+    "sapphire": "0",
+    "emerald": "0"
+  }
+]
