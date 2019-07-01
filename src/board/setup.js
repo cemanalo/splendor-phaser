aa91fd0a75a4b0018a33631e3c0e7a4674a0c0d4
Eugene.Manalo <eugene.manalo@EugeneManalos-MacBook-Pro.local>
2019-07-01 09:09:09 +0800

    Set up for 3 level cards

--- src/board/setup.js
+++ src/board/setup.js
@@ -5,15 +5,24 @@ import Card from '../components/card'
 import Noble from '../components/noble'
 
 import Level1Cards from '../components/card/contents/level_1'
+import Level2Cards from '../components/card/contents/level_2'
+import Level3Cards from '../components/card/contents/level_3'
 
 export default (scene) => {
   const shuffledL1Cards = shuffle(Level1Cards)
+  const shuffledL2Cards = shuffle(Level2Cards)
+  const shuffledL3Cards = shuffle(Level3Cards)
+
   const xs = 20
   const ys = 120
+
   console.log(shuffledL1Cards)
   for(var i = 0; i < 4; i++) {
     new Card(scene, xs + (120 * i), ys, shuffledL1Cards.pop())
+    new Card(scene, xs + (120 * i), ys + 150, shuffledL2Cards.pop())
+    new Card(scene, xs + (120 * i), ys + 300, shuffledL3Cards.pop())
   }
+
 
   console.log(shuffledL1Cards)
 }
